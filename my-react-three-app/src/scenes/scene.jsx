99f776ce644b6afca4e3925cbe4b6b9f7874b533
JavierFananasNaya <javierfananasnaya@gmail.com>
2023-10-23 21:32:15 +0200

    Added some meshes to the scene but camera not working

--- my-react-three-app/src/scenes/scene.jsx
+++ my-react-three-app/src/scenes/scene.jsx
@@ -1,16 +1,26 @@
 import React, { useRef } from 'react';
 import { Canvas } from 'react-three-fiber';
-
+import { angleToRadians } from '../utils/angle';
+import { PerspectiveCamera } from 'drei';
 function Scene() {
-  const boxRef = useRef();
+  const sphereRef = useRef();
 
   return (
+    <>
     <Canvas>
-      <mesh ref={boxRef}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color="red" />
+      {/* <PerspectiveCamera makeDefault position={[angleToRadians(90),0,0]}></PerspectiveCamera> */}
+      <mesh ref={sphereRef}>
+        <sphereGeometry args={[1, 32,32]} />
+        <meshStandardMaterial color="red" />
       </mesh>
+      <mesh rotation={[(angleToRadians(75)),0,0]}>
+        <planeGeometry args={[7,7]}></planeGeometry>
+        <meshStandardMaterial color="Pink"></meshStandardMaterial>
+      </mesh>
+      {/* ambient light */}
+      <ambientLight args={["#ffffff", 1]}></ambientLight>
     </Canvas>
+    </>
   );
 }
 
