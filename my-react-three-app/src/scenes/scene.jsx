23b0c7bac22c0f376735b1de57609d18e60c5b18
JavierFananasNaya <javierfananasnaya@gmail.com>
2023-11-06 21:08:11 +0100

    First Character controller prototype

--- my-react-three-app/src/scenes/scene.jsx
+++ my-react-three-app/src/scenes/scene.jsx
@@ -6,6 +6,7 @@ import {
   PerspectiveCamera,
 } from "@react-three/drei";
 import * as THREE from "three";
+import Ball from "../Objects/sphereCharacter";
 function Scene() {
   const sphereRef = useRef();
 
@@ -13,25 +14,23 @@ function Scene() {
     <>
       <PerspectiveCamera makeDefault position={[0, 1, 5]} />
       <OrbitControls />
-      {/* Ball */}
-      <mesh castShadow ref={sphereRef} position={[0, 0.5, 0]}>
-        <sphereGeometry args={[0.5, 32, 32]} />
-        <meshStandardMaterial color="#ffffff" metalness={3}/>
-      </mesh>
+      {/* Ball controlled character */}
+      
+      <Ball  position={[0, 0.5, 0]}/>
       {/* Floor */}
       <mesh rotation={[-angleToRadians(90), 0, 0]}>
         <planeGeometry args={[20, 20]}></planeGeometry>
         <meshPhongMaterial color="#2266cc" metalness={1}></meshPhongMaterial>
       </mesh>
       {/* ambient light */}
-      {/* <ambientLight args={["#ffffff", 0.25]}></ambientLight> */}
+      <ambientLight args={["#ffffff", 0.25]}></ambientLight>
       <spotLight
         args={["#ffffff", 3, 7, angleToRadians(45), 0.4]}
         position={[-3, 1, 0]}
         castShadow
       ></spotLight>
       {/* Environment */}
-      <Environment background>
+      {/* <Environment background>
         <mesh >
           <sphereGeometry args={[50, 100, 100]}></sphereGeometry>
           <meshBasicMaterial
@@ -39,7 +38,7 @@ function Scene() {
             color="Pink"
           ></meshBasicMaterial>
         </mesh>
-      </Environment>
+      </Environment> */}
     </>
   );
 }
