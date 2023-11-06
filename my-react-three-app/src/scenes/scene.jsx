a85563496f8701785f0e77510aa33069cb3d1a07
JavierFananasNaya <javierfananasnaya@gmail.com>
2023-11-06 20:32:42 +0100

    Added environment

--- my-react-three-app/src/scenes/scene.jsx
+++ my-react-three-app/src/scenes/scene.jsx
@@ -1,27 +1,45 @@
-import React, { useRef } from 'react';
-import { angleToRadians } from '../utils/angle';
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import React, { useRef } from "react";
+import { angleToRadians } from "../utils/angle";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import * as THREE from "three";
 function Scene() {
   const sphereRef = useRef();
 
   return (
     <>
-      <PerspectiveCamera makeDefault position={[0,1,5]}/>
-      <OrbitControls/>
+      <PerspectiveCamera makeDefault position={[0, 1, 5]} />
+      <OrbitControls />
       {/* Ball */}
-      <mesh castShadow ref={sphereRef} position={[0,0.5,0]}>
-        <sphereGeometry args={[0.5,32,32]} />
-        <meshStandardMaterial color="red" />
+      <mesh castShadow ref={sphereRef} position={[0, 0.5, 0]}>
+        <sphereGeometry args={[0.5, 32, 32]} />
+        <meshStandardMaterial color="#ffffff" metalness={3}/>
       </mesh>
-      <mesh receiveShadow rotation={[-(angleToRadians(90)),0,0]}>
-        <planeGeometry args={[7,7]}></planeGeometry>
-        <meshPhongMaterial color="Pink"></meshPhongMaterial>
+      {/* Floor */}
+      <mesh rotation={[-angleToRadians(90), 0, 0]}>
+        <planeGeometry args={[20, 20]}></planeGeometry>
+        <meshPhongMaterial color="#2266cc" metalness={1}></meshPhongMaterial>
       </mesh>
       {/* ambient light */}
-      <ambientLight args={["#ffffff", 0.1]}></ambientLight>
-      {/* <pointLight args={['#ffffff', 1]} position={[-3,2,0]}></pointLight> */}
-      <spotLight args={['#ffffff', 1.5,7, angleToRadians(45),0.4]} position={[-3,1,0]} castShadow></spotLight>
-
+      {/* <ambientLight args={["#ffffff", 0.25]}></ambientLight> */}
+      <spotLight
+        args={["#ffffff", 3, 7, angleToRadians(45), 0.4]}
+        position={[-3, 1, 0]}
+        castShadow
+      ></spotLight>
+      {/* Environment */}
+      <Environment background>
+        <mesh >
+          <sphereGeometry args={[50, 100, 100]}></sphereGeometry>
+          <meshBasicMaterial
+            side={THREE.BackSide}
+            color="Pink"
+          ></meshBasicMaterial>
+        </mesh>
+      </Environment>
     </>
   );
 }
