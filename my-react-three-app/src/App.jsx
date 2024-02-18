348fa04b8f0f60eca87d2e97f43422faed3694ec
JavierFananasNaya <javierfananasnaya@gmail.com>
2024-02-18 19:28:31 +0100

    Adding a Start game menu WIP

---
+++ my-react-three-app/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./App.scss";
+import DreiScene from "./scenes/dreiScene";
+const App = () => {
+  const [isGameActive, setGameActive] = useState(false);
+
+  const startGame = () => {
+    setGameActive(true);
+  };
+
+  return (
+    <div className="App">
+      {!isGameActive && (
+        <div className="mainMenu">
+          <h1>The Maze Reacter</h1>
+          <button className="playButton" onClick={startGame}>Play</button>
+        </div>
+      )}
+      {isGameActive && <DreiScene />}
+    </div>
+  );
+};
+
+export default App;
