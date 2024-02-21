11189fda10bac5379038b278cb4020c7b091184f
JavierFananasNaya <javierfananasnaya@gmail.com>
2024-02-21 19:15:15 +0100

    Added dirt texture to the ground

--- my-react-three-app/src/Objects/ground.jsx
+++ my-react-three-app/src/Objects/ground.jsx
@@ -1,16 +1,31 @@
-import diffuse from "../assets/ground/diffuse_ground.jpg";
-import normals from "../assets/ground/normal_ground.jpg";
+// import diffuse from "../assets/ground/diffuse_ground.jpg";
+// import normals from "../assets/ground/normal_ground.jpg";
+import diffuse from "../assets/dirt/Ground_Dirt_007_basecolor_2.jpg";
+import normals from "../assets/dirt/Ground_Dirt_007_normal.jpg";
+import ambientOclussion from "../assets/dirt/Ground_Dirt_007_ambientOcclusion.jpg";
+import roughness from "../assets/dirt/Ground_Dirt_007_roughness.jpg";
+import displacement from "../assets/dirt/Ground_Dirt_007_height.png";
+
 import { useTexture } from "@react-three/drei";
 import { CuboidCollider, RigidBody } from "@react-three/rapier";
 import { setTextureTiling } from "../utils/texture_utils.js";
 
 export function Ground(props) {
-  let [colorMap, normalMap] = useTexture([diffuse, normals]);
+  let [colorMap, normalMap, aoMap, roughnessMap, displacementMap] = useTexture([
+    diffuse,
+    normals,
+    ambientOclussion,
+    roughness,
+    displacement,
+  ]);
 
-  const tilingScale = 1000;
+  const tilingScale = 900;
 
   colorMap = setTextureTiling(colorMap, tilingScale);
   normalMap = setTextureTiling(normalMap, tilingScale);
+  aoMap = setTextureTiling(aoMap, tilingScale);
+  roughnessMap = setTextureTiling(roughnessMap, tilingScale);
+  displacementMap = setTextureTiling(displacementMap, tilingScale);
 
   return (
     <RigidBody {...props} type="fixed" colliders={false}>
@@ -20,6 +35,9 @@ export function Ground(props) {
           attach="material"
           map={colorMap}
           normalMap={normalMap}
+          aoMap={aoMap}
+          roughnessMap={roughnessMap}
+          displacementMap={displacementMap}
         />
       </mesh>
       <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
