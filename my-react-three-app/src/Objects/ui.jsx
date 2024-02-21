fa2de3daa6291a63d0e0cccfa85102a1e80e2d54
JavierFananasNaya <javierfananasnaya@gmail.com>
2024-02-21 18:51:20 +0100

    Added font from google font

--- my-react-three-app/src/Objects/ui.jsx
+++ my-react-three-app/src/Objects/ui.jsx
@@ -1,25 +1,11 @@
 import React, { useContext } from "react";
 import { pickUpsContext } from "../Contexts/pick_ups_context.tsx";
-
+import "./ui.scss";
 const Ui = () => {
   const { pickUpCount, pickUps } = useContext(pickUpsContext);
   return (
     <>
-      {
-        <div
-          style={{
-            color: "white", // Set text color
-            padding: "10px",
-            backgroundColor: "rgba(0, 0, 0, 0.5)", // Optional: add background color
-            borderRadius: "8px", // Optional: add border radius
-            position: "absolute",
-            top: "10vh",
-            left: "10vw",
-          }}
-        >
-          {`${pickUpCount} of ${pickUps.length} Books`}
-        </div>
-      }
+      {<div className="ui">{`${pickUpCount} of ${pickUps.length} Books`}</div>}
     </>
   );
 };
